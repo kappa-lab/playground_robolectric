9b7222b2c5e22a0f67a600199bebcfd2081fbbb5
yamagami kenichi <gamiken@kappa-lab.com>
2021-01-07 12:10:03 +0900

    setup robolectric

---
+++ app/src/test/java/com/kappalab/playground/robolectric/NotifierTest.kt
@@ -0,0 +1,13 @@
+package com.kappalab.playground.robolectric
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+
+@RunWith(RobolectricTestRunner::class)
+class NotifierTest{
+    @Test
+    fun setup(){}
+}
+
