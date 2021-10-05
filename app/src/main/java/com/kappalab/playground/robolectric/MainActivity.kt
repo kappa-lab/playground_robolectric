1557e465b05014d22a1a6246f49069488b423fed
yamagami kenichi <gamiken@kappa-lab.com>
2021-10-05 14:12:19 +0900

    add MyService

--- app/src/main/java/com/kappalab/playground/robolectric/MainActivity.kt
+++ app/src/main/java/com/kappalab/playground/robolectric/MainActivity.kt
@@ -1,27 +1,34 @@
 package com.kappalab.playground.robolectric
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("MainActivity", "Init")
+        Log.d(TAG, "Init")
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        BluetoothNotifier(application){
-            Log.i("MainActivity", "callback Bluetooth $it")
+        BluetoothNotifier(application) {
+            Log.i(TAG, "callback Bluetooth $it")
         }
 
-        TelephoneNotifier(application){
-            Log.i("MainActivity", "callback Telephone $it")
+        TelephoneNotifier(application) {
+            Log.i(TAG, "callback Telephone $it")
         }
 
-        AirPlaneModeNotifier(application){
-            Log.i("MainActivity", "callback AirPlane $it")
+        AirPlaneModeNotifier(application) {
+            Log.i(TAG, "callback AirPlane $it")
         }
 
+
+        startService(Intent(this@MainActivity, MyService::class.java))
     }
 }
---
+++ app/src/main/java/com/kappalab/playground/robolectric/MyService.kt
@@ -0,0 +1,22 @@
+package com.kappalab.playground.robolectric
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+
+class MyService : Service() {
+    companion object {
+        private const val TAG = "MyService"
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        Log.i(TAG, "onBind: $intent")
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        Log.i(TAG, "onStartCommand: $intent")
+        return super.onStartCommand(intent, flags, startId)
+    }
+}
