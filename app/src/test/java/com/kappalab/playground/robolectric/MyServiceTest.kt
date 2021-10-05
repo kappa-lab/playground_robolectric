88e46d70b26d5eeb02eda7a3320a150361315fa5
yamagami kenichi <gamiken@kappa-lab.com>
2021-10-05 14:53:38 +0900

    add test

---
+++ app/src/test/java/com/kappalab/playground/robolectric/MyServiceTest.kt
@@ -0,0 +1,23 @@
+package com.kappalab.playground.robolectric
+
+import android.app.Application
+import android.content.Intent
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.shadows.ShadowLooper
+
+@RunWith(RobolectricTestRunner::class)
+class MyServiceTest {
+    @Test
+    fun testStarted() {
+        val app = ApplicationProvider.getApplicationContext<Application>()
+
+        Assert.assertEquals(false, MyService.started)
+        app.startService(Intent(app, MyService::class.java))
+        ShadowLooper.shadowMainLooper().idle()
+        Assert.assertEquals(true, MyService.started)
+    }
+}
